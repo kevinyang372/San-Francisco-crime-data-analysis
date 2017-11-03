30da18fab11b8442e7634cc10974d66d9bbc2a95
kevinyang372 <kevin_yang_372@163.com>
2017-11-03 14:13:16 -0700

    SVM

--- run.py
+++ run.py
@@ -4,14 +4,14 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from datasource import load_data
 from data_processing import filterdata
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.multioutput import MultiOutputClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
 
 #Select Datasource
 filesource = ["train.csv","test.csv"]
 
 print("Loading Data...")
-file = load_data(filesource[0])
+file = load_data(filesource[0])[:100]
 #Split the Dataset into train set and test set
 train_data, test_data = train_test_split(file,test_size = 0.2,random_state=42)
 
@@ -19,22 +19,22 @@ train_data, test_data = train_test_split(file,test_size = 0.2,random_state=42)
 y_train, train_tr = filterdata(train_data)
 y_test, test_tr = filterdata(test_data)
 
-#Select Random Forest As the Model
+#Select SVM As the Model
 print("Training...")
-forest = RandomForestClassifier(n_estimators=100, random_state=1)
-multi_target_forest = MultiOutputClassifier(forest, n_jobs=-1)
-tree_reg = multi_target_forest.fit(train_tr, y_train)
+from sklearn.svm import SVC
+SVM_cls = OneVsRestClassifier(SVC(kernel="poly", degree=3, coef0=1 , C=5)).fit(train_tr, y_train)
 
 #Prediction
 print("Training Complete")
+print(SVM_cls)
 print("Predicting...")
-prediction = tree_reg.predict(test_tr)
+prediction = SVM_cls.predict(train_tr)
 
 #precision_score and recall_score
 from sklearn.metrics import precision_score, recall_score, f1_score
-p1 = precision_score(prediction,y_test,average="macro")
+p1 = precision_score(prediction,y_train,average="macro")
 print("Precision score (macro): ",p1)
-p2 = recall_score(prediction, y_test,average="macro")
+p2 = recall_score(prediction, y_train,average="macro")
 print("Recall score (macro): ",p2)
-p3 = f1_score(prediction,y_test,average="macro")
+p3 = f1_score(prediction,y_train,average="macro")
 print("F1 score (macro): ",p3)
