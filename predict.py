5858bac043e9c12027fdb24490c848a44c4ae371
kevinyang372 <kevin_yang_372@163.com>
2017-11-27 14:45:17 -0800

    predict

--- predict.py
+++ predict.py
@@ -1,17 +1,35 @@
 from statsmodels.tsa.arima_model import ARIMAResults
-from location_based_ARIMA import run
+from location import run
 from datasource import load_data
 
+#load dataset
 file = load_data("train.csv")
 
-cat = input("input the category you want to predict (Theft, Assault, Drug, Sex Offenses, Other Offense)")
+#take input
+cat = input("input the category you want to predict (Theft, Assault, Drug, Sex Offenses, Other Offenses)")
+
+if cat.lower() == "theft":
+	cat = "Theft"
+elif cat.lower() == "assault":
+	cat = "Assault"
+elif cat.lower() == "drug":
+	cat = "Drug"
+elif cat.lower() == "sex offenses":
+	cat = "Sexoffense"
+elif cat.lower() == "other offenses":
+	cat = "Offenses"
+
+#find model
 name = cat + '.pkl'
 
+#get latitude and longtitude
 location_x = float(input('latitude'))
 location_y = float(input('longtitude'))
 
+#get location possibility
 possibility = run(location_x,location_y,file)
 
+#predict
 model = ARIMAResults.load(name)
 raw = model.forecast()[0]
 
